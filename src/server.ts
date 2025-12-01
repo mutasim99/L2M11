9941a7835f72a882286efc2e050733501386d957
mutasim99 <virginmr882@gmail.com>
2025-12-01 07:53:05 +0600

    add a post method

--- src/server.ts
+++ src/server.ts
@@ -1,44 +1,46 @@
-// import http, { IncomingMessage, Server, ServerResponse } from 'http'
-// const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-//     console.log('server is running.....');
-
-//     if (req.url === '/' && req.method === 'GET') {
-//         res.writeHead(200, { 'content-type': 'application/json' });
-//         res.end(
-//             JSON.stringify({
-//                 message: 'hi i am create a sever using raw node js',
-//                 path: req.url
-//             })
-//         )
-//     };
-// });
-
-// server.listen(5000, () => {
-//     console.log(`My server is running on port:${5000}`);
-
-// });
-
-
-
-import http, { IncomingMessage, Server, ServerResponse } from 'http';
-import config from './config';
-
+import http, { IncomingMessage, Server, ServerResponse } from 'http'
 const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-    console.log('My server is running');
+    console.log('server is running.....');
 
     if (req.url === '/' && req.method === 'GET') {
-        res.writeHead(200, { 'content-type': 'application/json' }),
+        res.writeHead(200, { 'content-type': 'application/json' });
+        res.end(
+            JSON.stringify({
+                message: 'hi i am create a sever using raw node js',
+                path: req.url
+            })
+        )
+    };
+
+    if (req.url === '/api/users' && req.method === 'POST') {
+        let body = '';
+
+        // collect data
+        req.on('data', chunk=>{
+            body += chunk.toString();
+        });
+
+        // when request is finished
+        req.on('end', ()=>{
+            const parsed = JSON.parse(body);
+
+            res.writeHead(200, {'content-type': 'application/json'});
             res.end(
                 JSON.stringify({
-                    message: 'hi i am mutasim i am create a server using raw node.js',
-                    path: '/'
+                    message:'post received',
+                    receivedData : parsed
                 })
             )
+        })
+
+    }else{
+        res.writeHead(404)
+        res.end('data is not found')
     }
+});
+
+server.listen(5000, () => {
+    console.log(`My server is running on port:${5000}`);
 
 });
 
-server.listen(config.port, () => {
-    console.log(`My server is running on port:${config.port}`);
-
-});
